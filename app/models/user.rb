06317fedf4457ea1c89981714436647d7a7879fd
Hu Chen <huch0003@gmail.com>
2013-08-05 00:37:06 +0800

    change font, add header and messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ActiveRecord::Base
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  # attr_accessible :title, :body
+
+  attr_accessible :email, :password, :password_confirmation, :remember_me
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Talentedio::Application.routes.draw do
-  devise_for :users
-
+  devise_for :users, path: '', path_names: {sign_in: 'login', sign_out: 'logout'}
   root :to => 'main#home'
 end
